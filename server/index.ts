4caee8b83fba824d546a6f8c119a12bdfd5ccd8e
mk <v.a.stekolschikov@gmail.com>
2022-02-06 10:06:07 +0200

    added showTempReport

--- server/index.ts
+++ server/index.ts
@@ -14,13 +14,14 @@ app.use(bodyParser.json())
 
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.get('/api/test/run', TestRoute.run)
+app.get('/api/test/showTempReport', TestRoute.showTempReport)
 app.post('/signup', UserRoute.signup)
 app.get('/getUsers', passport.authenticate('jwt',{session: false}),UserRoute.getUsers)
 app.post('/login', UserRoute.login)
 app.get('/me', passport.authenticate('jwt',{session: false}), UserRoute.me)
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-});
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
--- server/routes/Test.ts
+++ server/routes/Test.ts
@@ -1,19 +1,16 @@
 import {Request, Response} from "express";
-
+import fs from "fs"
+import StatusCodes from "http-status-codes";
 const {exec} = require("child_process");
+const {BAD_REQUEST} = StatusCodes;
 
 const run = async (req: Request, res: Response) => {
-
-    // exec("pwd", (error: any, stdout: any, stderr: any) => { > report.txt
     exec( "mocha --timeout 10000 --require ts-node/register test/**/*_test.ts > report.txt", (error: any, stdout: any, stderr: any) => {
-    // exec("mocha --timeout 10000 --require ts-node/register test/1_all_offers.ts", (error: any, stdout: any, stderr: any) => {
         if (error) {
             console.log(`error: ${error.message}`);
-            // return;
         }
         if (stderr) {
             console.log(`stderr: ${stderr}`);
-            // return;
         }
         console.log(`stdout: ${stdout}`);
     });
@@ -21,4 +18,11 @@ const run = async (req: Request, res: Response) => {
     res.send("run2")
 }
 
-export {run}
+const showTempReport = async (req: Request, res: Response) => {
+    fs.readFile("report.txt", 'utf8', (err, data) => {
+        if (err) res.status(BAD_REQUEST).send(data)
+        res.status(200).send(data)
+    });
+}
+
+export {run, showTempReport}
